569818f1214af986ddf1ac51d9e418959e213429
Sophia DiPietro <sophiad.913@gmail.com>
2026-03-17 00:32:18 -0400

    Update GraphDriver.java

--- src/GraphDriver.java
+++ src/GraphDriver.java
@@ -1,7 +1,4 @@
 /********************************************************************************************
-* Sophia DiPietro (T00714296)
-* 11/22/2024
-*
 * GraphDriver.java
 * 
 * The GraphDriver class demonstrates the funcionality of the Graph class. 
